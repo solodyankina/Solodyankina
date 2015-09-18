b752b72d7db235cf985ccb5bc46a47de206a2ab4
Solodyankina Elena <solodyankina.len@mail.ru>
2015-09-19 02:29:25 +0500

    Add homework1

---
+++ ConsoleApplication5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace HomeWork
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var str = Clipboard.GetText().Split(',', ';', ':', '!', '?', '.', '\n', '\r', '\t', ' ', '<', '>', ')', '(', '{', '}', '-');
+            str.Distinct().OrderBy(x => x, StringComparer.OrdinalIgnoreCase). Where(x => !string.IsNullOrEmpty(x)).Take(1000).ToList().ForEach(Console.WriteLine);
+            Console.ReadLine();
+        }
+    }
+}
