5373a5a8ac56d3829d89e67f13849bb8a912ba25
solodyankina <solodyankina.len@mail.ru>
2015-12-25 20:03:43 +0500

    Bugs upd

---
+++ Bugs/Bugs/Form1.Designer.cs
@@ -0,0 +1,380 @@
+﻿namespace Bugs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.balance1 = new System.Windows.Forms.Label();
+            this.balance2 = new System.Windows.Forms.Label();
+            this.balance3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(439, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Игроки";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(485, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Третий";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(440, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Второй";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(387, 32);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Первый";
+            // 
+            // balance1
+            // 
+            this.balance1.AutoSize = true;
+            this.balance1.Location = new System.Drawing.Point(19, 16);
+            this.balance1.Name = "balance1";
+            this.balance1.Size = new System.Drawing.Size(31, 13);
+            this.balance1.TabIndex = 5;
+            this.balance1.Text = "1000";
+            // 
+            // balance2
+            // 
+            this.balance2.AutoSize = true;
+            this.balance2.Location = new System.Drawing.Point(68, 16);
+            this.balance2.Name = "balance2";
+            this.balance2.Size = new System.Drawing.Size(31, 13);
+            this.balance2.TabIndex = 6;
+            this.balance2.Text = "1000";
+            // 
+            // balance3
+            // 
+            this.balance3.AutoSize = true;
+            this.balance3.Location = new System.Drawing.Point(117, 16);
+            this.balance3.Name = "balance3";
+            this.balance3.Size = new System.Drawing.Size(31, 13);
+            this.balance3.TabIndex = 7;
+            this.balance3.Text = "1000";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(46, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(62, 19);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(46, 20);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(111, 19);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(46, 20);
+            this.textBox3.TabIndex = 10;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(111, 45);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(46, 20);
+            this.textBox4.TabIndex = 13;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(62, 45);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(46, 20);
+            this.textBox5.TabIndex = 12;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(13, 45);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(46, 20);
+            this.textBox6.TabIndex = 11;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(111, 97);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(46, 20);
+            this.textBox7.TabIndex = 19;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(62, 97);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(46, 20);
+            this.textBox8.TabIndex = 18;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(13, 97);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(46, 20);
+            this.textBox9.TabIndex = 17;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(111, 71);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(46, 20);
+            this.textBox10.TabIndex = 16;
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(62, 71);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(46, 20);
+            this.textBox11.TabIndex = 15;
+            // 
+            // textBox12
+            // 
+            this.textBox12.Location = new System.Drawing.Point(13, 71);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(46, 20);
+            this.textBox12.TabIndex = 14;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox7);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.textBox8);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox9);
+            this.groupBox1.Controls.Add(this.textBox6);
+            this.groupBox1.Controls.Add(this.textBox10);
+            this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Controls.Add(this.textBox11);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.textBox12);
+            this.groupBox1.Location = new System.Drawing.Point(377, 100);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(171, 129);
+            this.groupBox1.TabIndex = 20;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ставки";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.balance2);
+            this.groupBox2.Controls.Add(this.balance1);
+            this.groupBox2.Controls.Add(this.balance3);
+            this.groupBox2.Location = new System.Drawing.Point(377, 61);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(171, 38);
+            this.groupBox2.TabIndex = 21;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Баланс";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(424, 243);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 22;
+            this.button1.Text = "Старт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = global::Bugs.Properties.Resources.finish;
+            this.pictureBox5.Location = new System.Drawing.Point(351, 119);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(20, 98);
+            this.pictureBox5.TabIndex = 24;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::Bugs.Properties.Resources.roach2;
+            this.pictureBox4.Location = new System.Drawing.Point(12, 197);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(29, 20);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 23;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Bugs.Properties.Resources.roach2;
+            this.pictureBox2.Location = new System.Drawing.Point(12, 145);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(29, 20);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 23;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Bugs.Properties.Resources.roach2;
+            this.pictureBox3.Location = new System.Drawing.Point(12, 171);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(29, 20);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 23;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Bugs.Properties.Resources.roach2;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 119);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(29, 20);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 23;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(560, 278);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Тараканьи бега";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label balance1;
+        private System.Windows.Forms.Label balance2;
+        private System.Windows.Forms.Label balance3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Bugs/Bugs/Form1.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Bugs
+{
+
+    public partial class Form1 : Form
+    {
+        public class Player
+        {
+            public int Bal = 1000;
+            public int b1, b2, b3, b4 = 0;              //ставки
+            public double part;
+        }
+        Player p1 = new Player();
+        Player p2 = new Player();
+        Player p3 = new Player();
+        public bool error;
+        public bool blck = true;
+        public int rndStep = 5;                         //ширина шага тараканов
+        public int num = 1;
+        public int summ = 0;
+        public Random rnd = new Random();
+
+        public void Stat()                              //обновление счета игроков
+        {
+            balance1.Text = p1.Bal.ToString();
+            balance2.Text = p2.Bal.ToString();
+            balance3.Text = p3.Bal.ToString();
+        }
+
+        public void Pay()                               //разделение и раздача выигрыша
+        {
+            int pp1, pp2, pp3, pp4;
+            pp1 = (p1.b1 + p2.b1 + p3.b1);
+            pp2 = (p1.b2 + p2.b2 + p3.b2);
+            pp3 = (p1.b3 + p2.b3 + p3.b3);
+            pp4 = (p1.b4 + p2.b4 + p3.b4);
+            summ = pp1 + pp2 + pp3 + pp4;
+            if (num == 1 && pp1 != 0)
+            {
+               p1.Bal += summ*p1.b1/pp1;
+               p2.Bal += summ*p2.b1/pp1;
+               p3.Bal += summ*p3.b1/pp1;
+            }
+            if (num == 2 && pp2 != 0)
+            {
+                p1.Bal += (summ*p1.b2 / pp2);
+                p2.Bal += (summ*p2.b2 / pp2);
+                p3.Bal += (summ*p3.b2 / pp2);
+            }
+            if (num == 3 && pp3 != 0)
+            {
+                p1.Bal += (summ*p1.b3 / pp3);
+                p2.Bal += (summ*p2.b3 / pp3);
+                p3.Bal += (summ*p3.b3 / pp3);
+            }
+            if (num == 4 && pp4 != 0)
+            {
+                p1.Bal += (summ*p1.b4 / pp4);
+                p2.Bal += (summ*p2.b4 / pp4);
+                p3.Bal += (summ*p3.b4 / pp4);
+            }
+        }
+
+        public void preStart()                          //сброс значений перед запуском
+        {
+            summ = 0;
+            p1.part = 0;
+            p2.part = 0;
+            p3.part = 0;
+            Point pnt = new Point();
+            pnt = pictureBox1.Location;
+            pnt.X = 12;
+            pictureBox1.Location = pnt;
+            pnt = pictureBox2.Location;
+            pnt.X = 12;
+            pictureBox2.Location = pnt;
+            pnt = pictureBox3.Location;
+            pnt.X = 12;
+            pictureBox3.Location = pnt;
+            pnt = pictureBox4.Location;
+            pnt.X = 12;
+            pictureBox4.Location = pnt;
+
+        }
+
+        public int betIn()                              //принятие ставок
+        {
+            int errCount = 0;
+                error = Int32.TryParse(textBox1.Text, out p1.b1);
+                if (error == false && textBox1.Text != "") {MessageBox.Show("Неправильно указана ставка"); errCount++;} ;
+                if (textBox1.Text == "") { p1.b1 = 0; };
+
+                error = Int32.TryParse(textBox2.Text, out p2.b1);
+                if (error == false && textBox2.Text != "") { MessageBox.Show("Неправильно указана ставка"); errCount++; };
+                if (textBox2.Text == "") { p2.b2 = 0; };
+
+                error = Int32.TryParse(textBox3.Text, out p3.b1);
+                if (error == false && textBox3.Text != "") { MessageBox.Show("Неправильно указана ставка"); errCount++; };
+                if (textBox3.Text == "") { p3.b1 = 0; };
+
+                error = Int32.TryParse(textBox6.Text, out p1.b2);
+                if (error == false && textBox6.Text != "") { MessageBox.Show("Неправильно указана ставка"); errCount++; };
+                if (textBox6.Text == "") { p1.b2 = 0; };
+
+                error = Int32.TryParse(textBox5.Text, out p2.b2);
+                if (error == false && textBox5.Text != "") { MessageBox.Show("Неправильно указана ставка"); errCount++; };
+                if (textBox5.Text == "") { p2.b2 = 0; };
+
+                error = Int32.TryParse(textBox4.Text, out p3.b2);
+                if (error == false && textBox4.Text != "") { MessageBox.Show("Неправильно указана ставка"); errCount++; };
+                if (textBox4.Text == "") { p3.b2 = 0; };
+
+                error = Int32.TryParse(textBox12.Text, out p1.b3);
+                if (error == false && textBox12.Text != "") { MessageBox.Show("Неправильно указана ставка"); errCount++; };
+                if (textBox12.Text == "") { p1.b3 = 0; };
+
+                error = Int32.TryParse(textBox11.Text, out p2.b3);
+                if (error == false && textBox11.Text != "") { MessageBox.Show("Неправильно указана ставка"); errCount++; };
+                if (textBox11.Text == "") { p2.b3 = 0; };
+
+                error = Int32.TryParse(textBox10.Text, out p3.b3);
+                if (error == false && textBox10.Text != "") { MessageBox.Show("Неправильно указана ставка"); errCount++; };
+                if (textBox10.Text == "") { p3.b3 = 0; };
+
+                error = Int32.TryParse(textBox9.Text, out p1.b4);
+                if (error == false && textBox9.Text != "") { MessageBox.Show("Неправильно указана ставка"); errCount++; };
+                if (textBox9.Text == "") { p1.b4 = 0; };
+
+                error = Int32.TryParse(textBox8.Text, out p2.b4);
+                if (error == false && textBox8.Text != "") { MessageBox.Show("Неправильно указана ставка"); errCount++; };
+                if (textBox8.Text == "") { p2.b4 = 0; };
+
+                error = Int32.TryParse(textBox7.Text, out p3.b4);
+                if (error == false && textBox7.Text != "") { MessageBox.Show("Неправильно указана ставка"); errCount++; };
+                if (textBox7.Text == "") { p3.b4 = 0; };
+
+                if (p1.Bal > (p1.b1 + p1.b2 + p1.b3 + p1.b4))
+                {
+                    p1.Bal -= (p1.b1 + p1.b2 + p1.b3 + p1.b4);
+                }
+                else { MessageBox.Show("У первого игрока недостаточно средств"); errCount++; };
+
+                if (p2.Bal > (p2.b1 + p2.b2 + p2.b3 + p2.b4))
+                {
+                    p2.Bal -= (p2.b1 + p2.b2 + p2.b3 + p2.b4);
+                }
+                else { MessageBox.Show("У второго игрока недостаточно средств"); errCount++; };
+
+                if (p3.Bal > (p3.b1 + p3.b2 + p3.b3 + p3.b4))
+                {
+                    p3.Bal -= (p3.b1 + p3.b2 + p3.b3 + p3.b4);
+                }
+                else { MessageBox.Show("У третьего игрока недостаточно средств"); errCount++; };
+
+            return errCount;
+        }
+        public int Winner()                             //присвоение победы
+        {
+            timer1.Enabled = false;
+            MessageBox.Show("Таракан номер "+num+" выиграл!");
+            return 0;
+        }
+        public void Run(PictureBox pict)                //движение тараканов
+        {
+            Point pnt = new Point();
+            pnt = pict.Location;
+            pnt.X += rnd.Next(rndStep)+1;
+            pict.Location = pnt;
+        }
+        public int compBugPos()                         //определение лидера
+        {
+            num = 1;
+            int max=pictureBox1.Location.X;
+            if (max < pictureBox2.Location.X)
+            {
+                max = pictureBox2.Location.X;
+                num = 2;
+            }
+            if (max < pictureBox3.Location.X)
+            {
+                max = pictureBox3.Location.X;
+                num = 3;
+            }
+            if (max < pictureBox4.Location.X)
+            {
+                max = pictureBox4.Location.X;
+                num = 4;
+            }
+            return max;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (betIn() == 0)
+            {
+                button1.Enabled = false;
+                blck = true;
+                Stat();
+                timer1.Enabled = true;
+                preStart();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            if (compBugPos() < pictureBox5.Location.X - 10)
+            {
+                Run(pictureBox1);
+                Run(pictureBox2);
+                Run(pictureBox3);
+                Run(pictureBox4);
+            }
+            else
+            {
+                
+                Pay();
+                Stat();
+                Winner();
+                button1.Enabled = true;
+            };
+        }
+    }
+}
